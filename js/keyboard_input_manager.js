57428946ea18e4b5d9551a8b1ee5a6dbac85d797
Rudra Patel <rudrapatel26122004@gmail.com>
2022-01-12 19:53:15 -0500

    Fixed decripted keybopard listener

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -35,25 +35,21 @@ KeyboardInputManager.prototype.listen = function () {
   var self = this;
 
   var map = {
-    38: 0, // Up
-    39: 1, // Right
-    40: 2, // Down
-    37: 3, // Left
-    75: 0, // Vim up
-    76: 1, // Vim right
-    74: 2, // Vim down
-    72: 3, // Vim left
-    87: 0, // W
-    68: 1, // D
-    83: 2, // S
-    65: 3  // A
+    ArrowUp: 0, // Up
+    ArrowRight: 1, // Right
+    ArrowDown: 2, // Down
+    ArrowLeft: 3, // Left
+    w: 0, // W
+    d: 1, // D
+    s: 2, // S
+    a: 3  // A
   };
 
   // Respond to direction keys
   document.addEventListener("keydown", function (event) {
     var modifiers = event.altKey || event.ctrlKey || event.metaKey ||
                     event.shiftKey;
-    var mapped    = map[event.which];
+    var mapped    = map[event.key];
 
     if (!modifiers) {
       if (mapped !== undefined) {
